d3759727a33d8624f08dc51166c876e2d1e4a5b8
Ammar_Khaled <ammarkhaled2003@gmail.com>
2026-04-12 23:08:16 +0200

    initial admin dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AdminDashboard } from './admin-dashboard/admin-dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'admin-dashboard', component: AdminDashboard }
+];
